3eff9a5d4febd09da274044ad3d1edb010aa4af4
Jose Leonardo Quiñones <56600373+joseleoo@users.noreply.github.com>
2020-01-26 10:54:45 +0100

    se añade jquery validate

--- FlexiGo/media/js/site.js
+++ FlexiGo/media/js/site.js
@@ -1,12 +1,38 @@
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-})
-(function () {
-    'use strict';
-    $("#frmCliente").validate({
-        rules: {
-            idcliente: { required: true, maxlength: 255 },
-            nif: { required: true }
+    jQuery.validator.setDefaults({
 
-        }
+        success: "valid"
     });
+    $("#save").on("click", function () {
+
+
+        jQuery.validator.addMethod("last", function (value, element) {
+            return this.optional(element) || /^[a-z|A-Z|0-9]+[^I]\s?[a-z|A-Z|]{1}$/.test(value);
+        }, "El nif debe termninar en letra");
+        $("#formClient").validate(
+            {
+
+                rules: {
+                    idClient: {
+
+                        required: true,
+                        minlength: 8,
+                        last: true,
+                    },
+
+                },
+                messages: {
+                    idClient: {
+                        required: 'campo requerido',
+                        minlength: jQuery.validator.format("Escriba por lo menos {0} caracteres!")
+                    }
+                }
+
+
+            }
+        )
+
+
+    })
+
+});
